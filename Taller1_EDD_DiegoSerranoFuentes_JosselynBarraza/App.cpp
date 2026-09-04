c37b739af02e43ca3ccd7220e88af3d1d2dcaaaf
diegoserrano-cloud <diego.serrano@alumnos.ucn.cl>
2026-09-04 00:39:17 -0400

    Implementación clase Nodo

--- Taller1_EDD_DiegoSerranoFuentes_JosselynBarraza/App.cpp
+++ Taller1_EDD_DiegoSerranoFuentes_JosselynBarraza/App.cpp
@@ -2,7 +2,19 @@
 #include <string>
 using namespace std;
 
+class Nodo {
+    private:
+        int dato;
+        Nodo* siguiente;
+    public:
+        Nodo(int valor) : dato(valor), siguiente(nullptr) {}
+        int getDato() { return dato; }
+        Nodo* getSiguiente() { return siguiente; }
+        void setSiguiente(Nodo* nodo) { siguiente = nodo; }
+}
+
 int main() {
     cout << "Hola Taller 1 "  << endl;
+
     return 0;
 }
