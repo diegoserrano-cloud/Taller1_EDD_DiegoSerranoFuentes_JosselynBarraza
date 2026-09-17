b4c5387f8c5522579cdda681781584530909d2aa
josselynbarraza-sys <josselyn.barraza@alumnos.ucn.cl>
2026-09-17 00:36:45 -0300

    implementación logica metodos derivar paciente y mostrar Estado (muestra los estados de los servicios con sus respectivos pacientes)

--- logica/Hospital.cpp
+++ logica/Hospital.cpp
@@ -1,4 +1,6 @@
 #include "Hospital.hpp"
+#include <iostream>
+using namespace std;
 
 Hospital::Hospital(){
     string servicios[8] = {"Urgencias", "Medicina General", "Cardiología", "Neurología", 
@@ -22,7 +24,7 @@ Hospital::Hospital(){
 NodoServicio* Hospital::buscarServicio(string nombre) const{
     NodoServicio* cursor = primerServicio;
     while(cursor != nullptr){
-        if(cursor->getServicio() == nombre){ //buscar una manera para que no influyan las mayusculas y minusculas
+        if(cursor->getServicio() == nombre){ //BUSCAR una manera para que no influyan las mayusculas y minusculas
             return cursor;
         }cursor = cursor->getNextServicio;
 
@@ -30,11 +32,32 @@ NodoServicio* Hospital::buscarServicio(string nombre) const{
 }
 
 void Hospital::derivarPaciente(Paciente p){
-    
+    string servicio = p.getServicio();
+    NodoServicio* nodoServicio = buscarServicio(servicio);
+    if(nodoServicio != nullptr){
+        NodoPaciente* nuevo = new NodoPaciente(p);
+        nuevo->setNext(nodoServicio->getPacienteLista());
+        nodoServicio->setPrimerPaciente(nuevo);
+    }else{
+        //Dar un mensaje de que no se encontro el servicio sin cout 
     }
+    
+}
 
 void Hospital::mostrarEstado(){
+    NodoServicio* cursor = primerServicio;
+    while(cursor!=nullptr){
+        cout<<cursor->getServicio<()<< ": " << endl;
+        NodoPaciente* paciente = cursor->getPacienteLista();
 
+        while(paciente!=nullptr){
+            cout<<"- "<< paciente->getPaciente().getNombre() << endl;
+
+            paciente = paciente->getNext();
+        }
+
+        cursor = cursor->getNextServicio();
+    }
 }
 
 void Hospital::mostrarServicio(string nombre){
